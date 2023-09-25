6f12e28f788439cccfafcd405d6db39b0e56e181
JONGGON <rlawhdrhs27@gmail.com>
2023-09-25 18:06:10 +0900

    completed

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -21,28 +21,21 @@ import {GiGiftOfKnowledge, GiSkills} from 'react-icons/gi';
 import AOS from 'aos';
 
 const nameFont = Sunflower({
-    weight: ['700'],
-    subsets: ['latin'],
-    display: 'swap',
+    weight: ['700'], subsets: ['latin'], display: 'swap',
 });
 
 const contentFont = Sunflower({
-    weight: ['500'],
-    subsets: ['latin'],
-    display: 'swap',
+    weight: ['500'], subsets: ['latin'], display: 'swap',
 });
 
 const developerFont = Martian_Mono({
-    weight: ['800'],
-    subsets: ['latin'],
-    display: 'swap',
+    weight: ['800'], subsets: ['latin'], display: 'swap',
 });
 
 function Home() {
     useEffect(() => {
         AOS.init({
-            duration: 700,
-            easing: 'ease-in-sine', // default easing for AOS animations
+            duration: 500, easing: 'ease-in-sine', // default easing for AOS animations
         });
         AOS.refresh();
 
@@ -52,154 +45,156 @@ function Home() {
         };
     }, []);
 
-    return (
-        <motion.div
-            className="mx-auto mb-[7%] mt-[7%] h-full w-[98%] w-full bg-white min-[640px]:w-3/5 min-[1024px]:w-2/5"
-            initial={{opacity: 0, scale: 0.0, y: '0%'}}
-            animate={{
-                opacity: [0.0, 0.1, 0.2, 0.4, 0.8, 1],
-                scale: [1, 1, 1, 1, 1, 1], // x : ["5%", "0%"], y: ["0%", "5%"]
-            }}
-            transition={{
-                ease: 'linear',
-                duration: 2.1,
-            }}
-        >
-            <div className="px-8 py-8">
-                <section className="flex items-center">
-                    <Image
-                        src={profilePic}
-                        alt="Picture of DeepFocuser"
-                        placeholder="blur"
-                        sizes="100vw"
-                        className="h-auto w-[50%] rounded-full object-cover min-[640px]:w-[35%]"
-                    />
-                    <div className="ml-2 min-[320px]:ml-4 min-[400px]:ml-12">
-                        <p
-                            className={`${nameFont.className} text-3xl font-bold min-[320px]:text-4xl`}
-                        >
-                            {generalData.name}
-                        </p>
-                        <p
-                            dangerouslySetInnerHTML={{
-                                __html: generalData.jobTitle,
-                            }}
-                            className={`${developerFont.className} mt-2 text-lg min-[320px]:text-xl`}
-                        ></p>
-                    </div>
-                </section>
-                <section className="mt-8">
+    return (<motion.div
+        className="mx-auto mb-[7%] mt-[7%] h-full w-[98%] w-full bg-white min-[640px]:w-3/5 min-[1024px]:w-2/5"
+        initial={{opacity: 0, scale: 0.0, y: '0%'}}
+        animate={{
+            opacity: [0.0, 0.1, 0.2, 0.4, 0.8, 1], scale: [1, 1, 1, 1, 1, 1], // x : ["5%", "0%"], y: ["0%", "5%"]
+        }}
+        transition={{
+            ease: 'linear', duration: 2.1,
+        }}
+    >
+        <div className="px-8 py-8">
+            <section className="flex items-center">
+                <Image
+                    src={profilePic}
+                    alt="Picture of DeepFocuser"
+                    placeholder="blur"
+                    sizes="100vw"
+                    className="h-auto w-[50%] rounded-full object-cover min-[640px]:w-[35%]"
+                />
+                <div className="ml-2 min-[320px]:ml-4 min-[400px]:ml-12">
                     <p
-                        className={`${contentFont.className} mb-4 flex items-center text-2xl text-slate-900`}
+                        className={`${nameFont.className} text-3xl font-bold min-[320px]:text-4xl`}
                     >
-                        <TfiCommentsSmiley className="mr-2"/>
-                        <span>About</span>
+                        {generalData.name}
                     </p>
-                    <div
-                        className="mt-2 text-slate-600"
+                    <p
                         dangerouslySetInnerHTML={{
-                            __html: generalData.about,
+                            __html: generalData.jobTitle,
                         }}
-                    ></div>
-                </section>
-                {contentData.map(({title, items}: Content, index) => {
-                    return (
-                        <section key={index} className="mt-8" data-aos="fade">
-                            <p
-                                className={`${contentFont.className} mb-4 flex items-center text-2xl text-slate-900`}
+                        className={`${developerFont.className} mt-2 text-lg min-[320px]:text-xl`}
+                    ></p>
+                </div>
+            </section>
+            <section className="mt-8">
+                <p
+                    className={`${contentFont.className} mb-4 flex items-center text-2xl text-slate-900`}
+                >
+                    <TfiCommentsSmiley className="mr-2"/>
+                    <span>About</span>
+                </p>
+                <div
+                    className="mt-2 text-slate-600"
+                    dangerouslySetInnerHTML={{
+                        __html: generalData.about,
+                    }}
+                ></div>
+            </section>
+            {contentData.map(({title, items}: Content, index) => {
+                return (<section key={index} className="mt-8">
+                    <p
+                        data-aos="fade"
+                        className={`${contentFont.className} mb-4 flex items-center text-2xl text-slate-900`}
+                    >
+                        {title === 'Work Experience' ? (<MdWorkOutline className="mr-2"/>) : (
+                            <BiBookContent className="mr-2"/>)}
+                        <span>{title}</span>
+                    </p>
+                    <div>
+                        {items.map((item, index) => {
+                            return (<div
+                                data-aos="fade"
+                                className="mt-4 rounded-xl border-4 p-2"
+                                key={index}
                             >
-                                {title === 'Work Experience' ? (
-                                    <MdWorkOutline className="mr-2"/>
-                                ) : (
-                                    <BiBookContent className="mr-2"/>
-                                )}
-                                <span>{title}</span>
-                            </p>
-                            <div>
-                                {items.map((item, index) => {
-                                    return (
-                                        <div className="mt-4 rounded-xl border-4 p-2" key={index}>
-                                            <div className="text-slate-700 text-sm">
-                                                {item.date}
-                                            </div>
-                                            <div>
-                                                <p className="mt-1 text-lg">{item.title}</p>
-                                                <p className="text-slate-600 mt-1">
-                                                    {item.subTitle}
-                                                </p>
-                                                {item.description ? (
-                                                    <p className="mt-2 text-slate-500">
-                                                        {item.description}
-                                                    </p>
-                                                ) : null}
-                                            </div>
-                                        </div>
-                                    );
-                                })}
+                                <div className="text-sm text-slate-700">
+                                    {item.date}
+                                </div>
+                                <div>
+                                    <p
+                                        dangerouslySetInnerHTML={{
+                                            __html: item.title,
+                                        }}
+                                    >
+                                    </p>
+                                    <p className="mt-1 text-slate-600 font-bold text-md">
+                                        {item.subTitle}
+                                    </p>
+                                    {item.description ? (<p className="text-slate-500"
+                                                            dangerouslySetInnerHTML={{
+                                                                __html: item.description,
+                                                            }}
+                                    >
+                                    </p>) : null}
+                                </div>
+                            </div>);
+                        })}
+                    </div>
+                </section>);
+            })}
+            <section className="mt-8 text-sm">
+                <p
+                    className={`${contentFont.className} mb-4 flex items-center text-2xl text-slate-900`}
+                    data-aos="fade"
+                >
+                    <GiSkills className="mr-2"/>
+                    <span>Professional Skills</span>
+                </p>
+                <div className="rounded-xl border-4 p-2" data-aos="fade">
+                    <Skills/>
+                </div>
+            </section>
+            <section className="mt-8 text-sm">
+                <p
+                    className={`${contentFont.className} mb-4 flex items-center text-2xl text-slate-900`}
+                    data-aos="fade"
+                >
+                    <GiGiftOfKnowledge className="mr-2"/>
+                    <span>Knowledges</span>
+                </p>
+                <div className="flex flex-wrap justify-center rounded-xl border-4 p-2"
+                     data-aos="fade">
+                    <Tag/>
+                </div>
+            </section>
+            <section className="mt-8 text-sm">
+                <p
+                    className={`${contentFont.className} mb-4 flex items-center text-2xl text-slate-900`}
+                    data-aos="fade"
+                >
+                    <TbPhoneCall className="mr-2"/>
+                    <span>Contact</span>
+                </p>
+                <div className="flex flex-col gap-2 rounded-xl border-4 p-2" data-aos="fade">
+                    {generalData.contacts.map((contact, index) => {
+                        return (<div
+                            className="flex justify-between pb-1"
+                            key={index}
+                        >
+                            <div className="ml-1 mr-4 w-full max-w-[100px] font-bold text-slate-600">
+                                {contact.label}
                             </div>
-                        </section>
-                    );
-                })}
-                <section className="mt-8 text-sm" data-aos="fade">
-                    <p
-                        className={`${contentFont.className} mb-4 flex items-center text-2xl text-slate-900`}
-                    >
-                        <GiSkills className="mr-2"/>
-                        <span>Professional Skills</span>
-                    </p>
-                    <div className="rounded-xl border-4 p-2">
-                        <Skills/>
-                    </div>
-                </section>
-                <section className="mt-8 text-sm" data-aos="fade">
-                    <p
-                        className={`${contentFont.className} mb-4 flex items-center text-2xl text-slate-900`}
-                    >
-                        <GiGiftOfKnowledge className="mr-2"/>
-                        <span>Knowledges</span>
-                    </p>
-                    <div className="flex flex-wrap justify-center rounded-xl border-4 p-2">
-                        <Tag/>
-                    </div>
-                </section>
-                <section className="mt-8 text-sm" data-aos="fade">
-                    <p
-                        className={`${contentFont.className} mb-4 flex items-center text-2xl text-slate-900`}
-                    >
-                        <TbPhoneCall className="mr-2"/>
-                        <span>Contact</span>
-                    </p>
-                    <div className="flex flex-col gap-2 rounded-xl border-4 p-2">
-                        {generalData.contacts.map((contact, index) => {
-                            return (
-                                <div
-                                    className="flex justify-between pb-1"
-                                    key={index}
+                            <div className="text-md text-slate-900">
+                                <Link
+                                    href={contact.href}
+                                    target="_blank"
+                                    rel="noreferrer noopener"
+                                    className="hover:bg-base-100 flex items-center justify-end text-blue-500 transition hover:scale-110"
                                 >
-                                    <div className="ml-1 mr-4 w-full max-w-[100px] font-bold text-slate-600">
-                                        {contact.label}
-                                    </div>
-                                    <div className="text-md text-slate-900">
-                                        <Link
-                                            href={contact.href}
-                                            target="_blank"
-                                            rel="noreferrer noopener"
-                                            className="hover:bg-base-100 flex items-center justify-end text-blue-500 transition hover:scale-110"
-                                        >
                                             <span className="mr-2">
                                                 {contact.value}
                                             </span>
-                                            <FaExternalLinkAlt/>
-                                        </Link>
-                                    </div>
-                                </div>
-                            );
-                        })}
-                    </div>
-                </section>
-            </div>
-        </motion.div>
-    );
+                                    <FaExternalLinkAlt/>
+                                </Link>
+                            </div>
+                        </div>);
+                    })}
+                </div>
+            </section>
+        </div>
+    </motion.div>);
 }
 
 export default memo(Home);
--- src/data/content.ts
+++ src/data/content.ts
@@ -1,62 +1,37 @@
 import {Content} from '@/types';
 
-export const contentData: Content[] = [
-    {
-        title: 'Work Experience',
-        items: [
-            {
-                title: '컴투버스',
-                subTitle: 'AI / Web 개발자',
-                date: '2022.06 - Present',
-                description: 'AI ~',
-            },
-            {
-                title: '현대오토에버',
-                subTitle: 'AI 개발자',
-                date: '2020.10 - 2022.06',
-                description: 'AI ~',
-            },
-            {
-                title: '알체라',
-                subTitle: 'AI 개발자',
-                date: '2019.04 - 2020.09',
-                description: 'AI ~',
-            },
-            {
-                title: '고영테크놀러지',
-                subTitle: 'AI 개발자',
-                date: '2018.01 - 2019.03',
-                description: 'AI ~',
-            }
-        ],
-    },
-    {
-        title: 'Education',
-        items: [
-            {
-                title: "Master's Degree in Robotics",
-                subTitle: 'University of Kwangwoon',
-                date: '2016 - 2018',
-                description: 'Specialized in MachineLearning/DeepLearning',
-            },
-            {
-                title: "Bachelor's Degree in Robotics",
-                subTitle: 'University of Kwangwoon',
-                date: '2010 - 2016',
-                description: 'Specialized in Intelligent System Control.',
-            },
-        ],
-    },
-    //   @NOTE: You can add more sections here
-    //   {
-    //     title: "Projects",
-    //     items: [
-    //       {
-    //         title: "Project 1",
-    //         subTitle: "Sub Title",
-    //         date: "2015 - 2016",
-    //         description: "Lorem ipsum dolor sit amet, consectetur adipiscing eli",
-    //       },
-    //     ],
-    //   },
-];
+export const contentData: Content[] = [{
+    title: 'Work Experience', items: [{
+        title: '<p class="text-lg mt-2 bg-gradient-to-r from-blue-400 to-fuchsia-600 bg-clip-text font-bold text-transparent">컴투버스</p>',
+        subTitle: 'AI / Web 개발자',
+        date: '2022.06 - Present',
+        description: '<p class="text-sm mt-1">1. Human Matting Algorithm 개발 및 유니티 적용</p>' + '<p class="text-sm mt-1">2. Face Landmark for Avatar Algorithm 개발 및 유니티 적용</p>' + '<p class="text-sm mt-1">3. Voice Activity Detection/ Voice Denoiser 개발 및 적용</p>' + `<p class="text-sm mt-1">4. Front End 신기술 조사 / Next.js(app 기반) 도입</p>` + '<p class="text-xs mt-2"># 팀내 study / 신기술 도입 주도</p><p class="text-xs mt-1"># 주니어 직원 과제 매니징 및 멘토 역할</p>',
+    }, {
+        title: '<p class="text-lg mt-2 bg-gradient-to-r from-blue-800 to-blue-300 bg-clip-text font-bold text-transparent">현대오토에버</p>',
+        subTitle: 'AI 개발자',
+        date: '2020.10 - 2022.06',
+        description: '<p class="text-sm mt-1">1. 드론 영상 기반 지붕 검사 알고리즘 개발</p>' + '<p class="text-sm mt-1">2. 모바일 PDA 문자인식 시스템 개발</p>' + '<p class="text-xs mt-2"># 팀내 study / 신기술 도입 주도</p><p class="text-xs mt-1"># 팀내 분위기 메이커</p>',
+    }, {
+        title: '<p class="text-lg mt-2 bg-gradient-to-r from-blue-900 to-sky-800 bg-clip-text font-bold text-transparent">알체라</p>',
+        subTitle: 'AI 개발자',
+        date: '2019.04 - 2020.09',
+        description: '<p class="text-sm mt-1">1. 행동 인식 딥러닝 알고리즘 개발</p>' + '<a href="https://medium.com/apache-mxnet/using-mxnet-to-detect-fire-d8afaf4d9e07" class="text-sm mt-1 bg-gray-100" target="_blank" rel="noreferrer noopener">2. 실내외 화재 탐지 딥러닝 알고리즘 개발</a>' + '<p class="text-xs mt-2"># 팀내 study / 신기술 도입 주도</p><p class="text-xs mt-1"># 팀내 분위기 메이커</p>',
+    }, {
+        title: '<p class="text-lg mt-2 bg-gradient-to-r from-green-600 to-green-300 bg-clip-text font-bold text-transparent">고영테크놀러지</p>',
+        subTitle: 'AI 개발자',
+        date: '2018.01 - 2019.03',
+        description: '<p class="text-sm mt-1">1. 딥러닝 알고리즘 기법을 적용한 문자 분류기 개발 및 제품에 적용</p>' + '<p class="text-sm mt-1">2. 딥러닝 알고리즘 기법을 적용한 반도체 이미지 3D Reconstruction 개발 및 제품에 적용</p>' + '<p class="text-xs mt-2"># 팀내 study / 신기술 도입 주도</p><p class="text-xs mt-1"># 팀내 막내로서 분위기 메이커</p>',
+    }],
+}, {
+    title: 'Education', items: [{
+        title: '<p class="text-lg font-bold mt-2 bg-gradient-to-r from-rose-800 to-rose-300 bg-clip-text text-transparent">광운대학교 대학원</p>',
+        subTitle: '로봇학과 머신러닝/딥러닝 전공',
+        date: '2016.03 - 2018.02',
+        description: '<p class="mt-1 text-sm">1. 평균 학점 : 3.79</p>' + '<p class="mt-1 text-sm">2. 졸업논문 : 드론을 이용한 1 인칭 텔레프리젠스 시스템(우수논문상)</p>'
+    }, {
+        title: '<p class="text-lg font-bold mt-2 bg-gradient-to-r from-rose-800 to-rose-300 bg-clip-text text-transparent">광운대학교</p>',
+        subTitle: '로봇학부 지능시스템제어 전공',
+        date: '2010.03 - 2016.02',
+        description: '<p class="text-sm mt-1">1. 평균 학점: 4.27<p><p class="mt-1 text-sm">2. 임베디드 SW 전문인력양성 과정 이수<p>' + '<p class="mt-1 text-sm">3. 졸업논문 : 순환 신경망 기반의 행동 예측 방법<p>',
+    },],
+},];
